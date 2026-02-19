8aa4ae3d9087cb43aa4675270f02563864817825
TechAdeptRDD <39470203+TechAdeptRDD@users.noreply.github.com>
2026-02-19 09:27:10 +0000

    fix(v0.8.1): manual reconstruction of Kotlin MainActivity and Manifest to force V2 compliance

---
+++ flutter_app/android/app/src/main/kotlin/com/reddcoin/reddmobile/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.reddcoin.reddmobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // In V2, this stays empty. The engine handles the heavy lifting.
+}
